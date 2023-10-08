08355e9da44ca8a128b5d7125967ac88c85ea9d1
KadalurSupreeth <kadalursupreeth@gmail.com>
2023-10-08 17:46:42 -0500

    Initial version

--- dependencies/asset2ce99f457dcc0e1255966c9394f1ef9e7355b1e6cacfc9f23913a624ec29bef4/jest.setup.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-process.on("unhandledRejection", (reason) => {
-    throw reason;
-});
-process.env.LOG_LEVEL = "none";
-process.env.SEND_METRIC = "Yes";
-process.env.SOLUTION_ID = "MyId";
-process.env.SOLUTION_UUID = "Uuid";
-process.env.METRICS_ENDPOINT = "MyEndpoint";
